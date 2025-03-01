a1244983c9f2f4fe442f54fe24348cfa1cdcb9c8
OfficeGaku <info@office-gaku.net>
2025-03-01 15:18:46 +0900

    設定画面をiOSに準拠したイメージに変更

---
+++ ArcheryRecord/SettingsDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsDetailView.swift
+//  ArcheryRecord
+//
+//  Created by オフィス岳 on 2025/03/01.
+//
+
+import SwiftUI
+
+struct SettingsDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsDetailView()
+}
